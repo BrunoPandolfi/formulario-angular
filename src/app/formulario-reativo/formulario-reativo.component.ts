b4593c8cc050cca563b7f80cdb7d04dd6831af3c
BrunoPandolfi <bf.pandolfi@hotmail.com>
2018-07-13 17:58:29 -0300

    Arquivos modificados conforme aula 97

--- src/app/formulario-reativo/formulario-reativo.component.ts
+++ src/app/formulario-reativo/formulario-reativo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '../../../node_modules/@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators} from '../../../node_modules/@angular/forms';
 import { map } from 'rxjs/operators';
 import { Http } from '@angular/http';
 
@@ -19,21 +19,58 @@ export class FormularioReativoComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    /*this.formulario = new FormGroup({
+    /* this.formulario = new FormGroup({
       nome: new FormControl(null),
       email: new FormControl(null)
     });*/
 
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required]
+      });
     });
   }
 
   onSubmit(){
-    console.log(this.formulario.value);
+    console.log(this.formulario);
     this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-             .pipe(map(res => res))
-             .subscribe(dados => console.log(dados));
+        .pipe(map(res => res))
+        .subscribe(dados => {
+          console.log(dados)
+          //this.resetar();
+        });
+    
+  }
+
+  verificaValidTouched (campo: string){
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+
+  verificaEmailInválido(){ 
+    let campoEmail = this.formulario.get('email');
+    if (campoEmail.get('email').errors)
+    {
+      return campoEmail.errors['email']
+    }
+  }
+
+  aplicaIsValidInvalid(campo: string)
+  {
+    return {
+      'is-valid' : this.verificaValidTouched(campo),
+      'is-invalid' : this.verificaValidTouched(campo)
+    };
+  }
+
+  resetar(){
+    this.formulario.reset();
   }
 }
